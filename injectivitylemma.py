c6e1112e1f91d91ba5dfdcb67fc5d08de837632e
Daira Hopwood <daira@jacaranda.org>
2020-02-18 09:33:36 +0000

    injectivitylemma.py: change variable names to match paper.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- injectivitylemma.py
+++ injectivitylemma.py
@@ -2,22 +2,22 @@
 
 from itertools import product
 
-def gamma_delta():
+def c_d():
     yield (-1,  0)
     yield ( 1,  0)
     yield ( 0, -1)
     yield ( 0,  1)
 
-def sums_mod4(gd):
-    return ((2**(k+1) + sum([gd[j][0] * (2**j) for j in range(k)])) % 4,
-            (2**(k+1) + sum([gd[j][1] * (2**j) for j in range(k)])) % 4)
+def sums_mod4(cd):
+    return ((2**(k+1) + sum([cd[j][0] * (2**j) for j in range(k)])) % 4,
+            (2**(k+1) + sum([cd[j][1] * (2**j) for j in range(k)])) % 4)
 
 for k in (1, 2):
-    M_k = [list(s) for s in product(gamma_delta(), repeat=k)]
+    M_k = [list(s) for s in product(c_d(), repeat=k)]
     assert(len(M_k) == 4**k)
 
-    for (gd, gd_dash) in product(M_k, repeat=2):
-        if gd[0] != gd_dash[0]:
-            assert(sums_mod4(gd) != sums_mod4(gd_dash))
+    for (cd, cd_dash) in product(M_k, repeat=2):
+        if cd[0] != cd_dash[0]:
+            assert(sums_mod4(cd) != sums_mod4(cd_dash))
 
 print("QED")
