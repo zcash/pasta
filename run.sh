cb6ef790f925995733222456bc12305b62aa33aa
Daira Hopwood <daira@jacaranda.org>
2020-11-19 21:54:44 +0000

    Update parameters for Pallas/Vesta.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ sage verify.sage Ep
 sage verify.sage Eq
 
 echo ""
-echo "Ep (Tweedledum)"
-echo "---------------"
+echo "Ep (Pallas)"
+echo "-----------"
 grep -Rn '.' Ep/verify-* |grep '^Ep/verify-.*:1:' |sed 's/:1:/ = /'
 echo ""
-echo "Eq (Tweedledee)"
-echo "---------------"
+echo "Eq (Vesta)"
+echo "----------"
 grep -Rn '.' Eq/verify-* |grep '^Eq/verify-.*:1:' |sed 's/:1:/ = /'
 
